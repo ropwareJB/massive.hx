549735bb209e512e1d5aebf099661da4a416510b
Montycarlo <joshua.brown@au.ey.com>
2017-01-16 22:36:05 +1100

    refactored extraction of fields from instance expression

--- src/Main.hx
+++ src/Main.hx
@@ -35,16 +35,7 @@ class ExampleModel{
   }
 
   public static macro function massive(a:Expr, b:Expr, ?props:Array<String>){
-    if(props == null){
-      var a_t:haxe.macro.Type = Context.typeof(a);
-      switch(a_t){
-        case TInst(class_t, params):
-           props = [ for(f in class_t.get().fields.get()) f.name ];
-        default: 
-           var e = 'Massive Assignment Error: Object instance expected instead of $a_t';
-           Context.error(e, a.pos);
-      }
-    }
+    props = (props == null) ? extractFields(a) : props;
 
     var aID:String = extrIdentifier(a);
     var bID:String = extrIdentifier(b);
@@ -53,6 +44,17 @@ class ExampleModel{
   }
 
 #if macro
+  private static function extractFields(x:Expr){
+    var x_type = Context.typeof(x);
+    switch(x_type){
+      case TInst(class_t, params):
+         return [ for(f in class_t.get().fields.get()) f.name ];
+      default: 
+         var e = 'Massive Assignment Error: Object instance expected instead of $x_type';
+         return Context.error(e, x.pos);
+    }
+  }
+
   private static function extrIdentifier(a:Expr){
     switch(a.expr){
       case EConst(CIdent(x)): return '$x';
