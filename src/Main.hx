2140cc57f88e23c543124967e133182d94f074c8
Montycarlo <joshua.brown@au.ey.com>
2017-01-16 22:21:33 +1100

    Automatic extraction of fields if not supplied

--- src/Main.hx
+++ src/Main.hx
@@ -17,25 +17,41 @@ class ExampleModel{
 
 @:final class Main{
 
-	public static function main():Void{
+  public static function main():Void{
     var oldModel = new ExampleModel(1, 2);
     var newModel = new ExampleModel(3, 4);
 
     // How I want it to end up looking?
     //@massive(propertyA) oldModel = newModel;
-    massive(oldModel, newModel, ["propertyA"]);
 
-    trace('Old propertyA: ${oldModel.propertyA}');
-    trace('Old propertyB: ${oldModel.propertyB}');
-	}
+    // Selective properties
+    massive(oldModel, newModel, ["propertyA", "propertyB"]);
 
-  public static macro function massive(a:Expr, b:Expr, props:Array<String>){
-    var aID = extrIdentifier(a);
-    var bID = extrIdentifier(b);
+    // Assign all properties that oldModel has
+    massive(oldModel, newModel);
+
+    trace('oldModel (reassigned) propertyA: ${oldModel.propertyA}');
+    trace('oldModel (reassigned) propertyB: ${oldModel.propertyB}');
+  }
+
+  public static macro function massive(a:Expr, b:Expr, ?props:Array<String>){
+    if(props == null){
+      var a_t:haxe.macro.Type = Context.typeof(a);
+      switch(a_t){
+        case TInst(class_t, params):
+           props = [ for(f in class_t.get().fields.get()) f.name ];
+        default: 
+           var e = 'Massive Assignment Error: Object instance expected instead of $a_t';
+           Context.error(e, a.pos);
+      }
+    }
+
+    var aID:String = extrIdentifier(a);
+    var bID:String = extrIdentifier(b);
     var code = '{\n' + [for(p in props) '$aID.$p = $bID.$p;'].join('\n') + '}';
     return Context.parse(code, Context.currentPos());
   }
-  
+
 #if macro
   private static function extrIdentifier(a:Expr){
     switch(a.expr){
